dbeeccac97637b58ff54694132ff3e05410232cf
Harley McDowell <hmcdowell95@gmail.com>
2020-02-07 02:17:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+
+
+class School 
+  attr_reader :school, :grade
+  attr_accessor :student
+  ROSTER = {}
+  
+  def initialize(school)
+    @school = school 
+  end
+  
+  def add_student(student, grade)
+    if ROSTER.include?(student)
+    elsif ROSTER.include?(grade)
+      ROSTER[grade] << student
+    else
+      ROSTER[grade] = []
+      ROSTER[grade] << student
+    end
+  end
+  
+  def roster
+    ROSTER
+  end
+  def grade=(grade)
+    @grade = grade
+    return ROSTER[grade]
+  end
+  def sort 
+    ROSTER.collect do |k, v|
+      v.sort
+    end
+    return ROSTER.sort
+  end
+end
